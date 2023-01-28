81f582f675b3e9ed55c8541c1cfa1ae79cb41e92
sassy alexandre <sassy.alexandre@gmail.com>
2023-01-28 21:00:26 +0100

    creation of round and match class (__init__ class constructor)

---
+++ models/match.py
@@ -0,0 +1,9 @@
+class match():
+    def __init__(self, player_1, score_player_1, player_2, score_player_2):
+        self.player_1 = player_1
+        self.score_player_1 = score_player_1
+        self.player_2 = player_2
+        self.score_player_2 = score_player_2
+        self.match_result = ([player_1, score_player_1], [player_2, score_player_2])
+
+    
---
+++ models/round.py
@@ -0,0 +1,6 @@
+class Round:
+    def __init__(self, name, beginning_date, ending_date):
+        self.name = name
+        self.beginning_date = beginning_date
+        self.ending_date = ending_date
+        self.match_list = []
