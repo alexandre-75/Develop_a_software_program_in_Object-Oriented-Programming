43ea2aaaec751c34fd91946dea0113c98d540344
sassy alexandre <sassy.alexandre@gmail.com>
2023-01-29 21:09:21 +0100

    creation of the database (players.json) + save function + checks function

--- models/player.py
+++ models/player.py
@@ -1,17 +1,25 @@
 import string
 from datetime import datetime
+from tinydb import TinyDB
 
 class Player():
+    
+    DB = TinyDB('database/players.json', indent=4)
+
     def __init__(self, first_name, surname, date_of_birth):
         self.first_name = first_name
         self.surname = surname
-        self.date_of_birth = date_of_birth
+        self.date_of_birth = str(date_of_birth)
     
     def __repr__(self):
         return f"Player({self.first_name}, {self.surname}, {self.date_of_birth})"
 
     def __str__(self):
         return f"(first name : {self.first_name}\n surname : {self.surname}\n date of birth : {self.date_of_birth})"
+
+    def checks(self):
+        self._check_first_name_and_surname()
+        self._check_date_of_birth()
 
     def _check_first_name_and_surname(self):
         special_characters = string.punctuation + string.digits
@@ -26,9 +34,15 @@ class Player():
 
     def _check_date_of_birth(self):
         try:
-            datetime.strptime(str(self.date_of_birth),'%Y-%m-%d')
+            datetime.strptime(self.date_of_birth,'%Y-%m-%d')
         except:
             raise ValueError("Invalid format. Use YYYY-MM-DD")
+    
+    def save(self, validate_data = False):
+        if validate_data:
+            self.checks()
+        return Player.DB.insert({"first_name":self.first_name, "surname":self.surname, "birth":self.date_of_birth})
+
 
 from faker import Faker
 fake = Faker(locale="fr_FR")
@@ -37,6 +51,7 @@ for _ in range(10):
     player = Player(first_name = fake.first_name(), surname = fake.last_name(), date_of_birth = fake.date_of_birth())
     player._check_first_name_and_surname()
     player._check_date_of_birth()
-    print(repr(player))
-    print("-" * 10)
-    print(player)
+    # print(repr(player))
+    # print("-" * 10)
+    # print(string.punctuation)
+    # print(player.save(validate_data = True))
