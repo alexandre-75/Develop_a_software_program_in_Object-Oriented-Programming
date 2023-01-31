c1e832296c1a7492be55fdcc96087ea01d682fd0
sassy alexandre <sassy.alexandre@gmail.com>
2023-01-31 15:52:04 +0100

    creation search_player_in_db function in player class

--- models/player.py
+++ models/player.py
@@ -1,6 +1,14 @@
 import string
 from datetime import datetime
-from tinydb import TinyDB
+from tinydb import TinyDB, Query, where
+"""
+TinyDB est une bibliothèque Python pour le stockage de données de petite taille de manière simple et rapide.
+
+Query est une classe qui permet d'exécuter des requêtes complexes sur les données stockées dans TinyDB.
+
+where est une fonction qui peut être utilisée pour filtrer les données stockées dans TinyDB en fonction d'un critère spécifié.
+"""
+
 
 class Player():
     
@@ -23,12 +31,19 @@ class Player():
         if validate_data:
             self.checks()
         return Player.db_player.insert({"first_name":self.first_name, "surname":self.surname, "birth":self.date_of_birth})
-    
+
+    def search_player_in_db(self, first_name):
+        players_db = TinyDB('database/players.json')
+        result = players_db.search(where('first_name') == first_name)   # Search in the database, the player corresponding to the name provided as input.
+        if result:
+            return result[0]
+        return None
+
     @staticmethod   # can be called without creating an instance of the class
     def load_all_player_in_db():
         players_db = TinyDB('database/players.json')
         return players_db.all()
-
+    
     def checks(self):
         self._check_first_name_and_surname()
         self._check_date_of_birth()
@@ -53,7 +68,7 @@ class Player():
 
 from faker import Faker
 fake = Faker(locale="fr_FR")
-for _ in range(10):
+for _ in range(1):
     player = Player(first_name = fake.first_name(), surname = fake.last_name(), date_of_birth = fake.date_of_birth())
     # player._check_first_name_and_surname()
     # player._check_date_of_birth()
@@ -61,5 +76,6 @@ for _ in range(10):
     # print("-" * 10)
     # print(string.punctuation)
     # print(player.save_player_in_db(validate_data = True))
-    print(player.load_player_db())
+    # print(player.load_all_player_in_db())
+    # print(player.search_player_in_db("Constance"))
 
