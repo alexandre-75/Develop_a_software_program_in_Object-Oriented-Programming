174fbfe4476644dc1a7758236c5c9ab9986a0183
sassy alexandre <sassy.alexandre@gmail.com>
2023-02-22 15:32:19 +0100

    reorganization

--- models/round.py
+++ models/round.py
@@ -1,105 +1,12 @@
 from tinydb import TinyDB, Query, where
-from utils.date_and_time import get_times
+# from utils.date_and_time import get_times
 # from models.match import Match
 
 class Round:
-
-    rounds_database = TinyDB("database/rounds.json", indent=4)
-
-    def __init__(self, round_name,):
-        self.round_name = round_name
-        self.start_date = ""
-        self.round_open = False
-        self.end_date = ""
-        self.round_closed = False
+    
+    def __init__(self, round_name, start_date, end_date="_"):
+        self.round_name = str(round_name)
+        self.start_date = str(start_date)
+        self.end_date = str(end_date)
         self.matches_list = []
-
- #---------------------------------------------------------------------------------------
- 
-    def __str__(self):
-        return f"Round name:{self.round_name} "
-    
-    def __repr__(self):
-        return f"Round({self.round_name})"
-
- #---------------------------------------------------------------------------------------
-
-    def close_round(self):
-        self.end_date = get_times()
-        self.round_closed = True
-        return self.horodatage_end
-
-    @property
-    def closed(self):
-        return self.round_closed
-
-    def round_open(self):
-        self.start_date = get_times()
-        self.round_open = True
-        return self.start_date
-
-    @property
-    def open(self):
-        return self.round_open
-
- #--------------------------------------------------------------------------------------- 
-
-    def add_match_in_round(self, player_1, score_p1, player_2, score_p2):
-        self.matches.append(Match(player_1, score_p1, player_2, score_p2))
-    
-    @property
-    def len_matches_list(self):
-        return len(self.matches)
-
- #--------------------------------------------------------------------------------------
-
-    def format_round_in_database(self):
-        return {
-                "round_name":self.round_name,
-                "start_date":self.start_date,
-                "end_start": self.end_date,
-        }
-
-    def save_round_in_database(self):
-            return self.rounds_database.insert(self.format_round_in_database())
-
- #---------------------------------------------------------------------------------------
-    
-    @staticmethod   # can be called without creating an instance of the class
-    def find_round_in_database(round_name):
-        rounds_database = TinyDB('database/rounds.json')
-        result = rounds_database.search(where("round_name") == round_name)   # Search in the database, the round corresponding to the id provided as input.
-        if result:
-            return result[0]
-        return "No round found in database"
-
-    @staticmethod
-    def update_round_in_database(key, value, round_name):
-        rounds_database = TinyDB('database/rounds.json')
-        round = Round.find_round_in_database(round_name)
-        if round:
-            return rounds_database.update({key:value}, where("round_name") == round_name)    
-        return "error in parameters"
-
-    @staticmethod
-    def load_all_rounds_from_database():
-        rounds_database = TinyDB('database/rounds.json')
-        return rounds_database.all()
-#------------------------------------------------------------------------------
-
-from faker import Faker
-fake = Faker(locale="fr_FR")
-test_r_id= [0, 1, 2, 3, 4]
-for _ in range(10):
-    round = Round(
-        round_name = fake.first_name(),
-        start_date= fake.date_of_birth(),
-        end_date = fake.date_of_birth())
-   
-    # print(round.__str__())
-    # print(round.__repr__())
-    # print(round.save_round_in_database())
-    # print(round.find_round_in_database("1"))
-    # print(round.load_all_rounds_from_database())
-    # print(round.update_round_in_database("round_name", "alex", 4))
-    # print("_"*10)
+        self.rounds_database = TinyDB("database/rounds.json", indent=4)
--- models/tournament.py
+++ models/tournament.py
@@ -2,48 +2,19 @@ from tinydb import TinyDB, Query, where
 
 class Tournament():
 
-    tournaments_database = TinyDB('database/tournaments.json', indent=4)
-
     def __init__(self,tournament_id, tournament_name, tournament_site, start_date, end_date, number_of_rounds=4, current_round=1, general_remarks="", players=[], rounds=[]):       
-        self.tournament_id = tournament_id
-        self.tournament_name = tournament_name
-        self.tournament_site = tournament_site
+        self.tournament_id = int(tournament_id)
+        self.tournament_name = str(tournament_name)
+        self.tournament_site = str(tournament_site)
         self.start_date = str(start_date)
         self.end_date = str(end_date)
-        self.number_of_rounds = number_of_rounds
-        self.current_round = current_round
-        self.general_remarks = general_remarks
+        self.number_of_rounds = int(number_of_rounds)
+        self.current_round = int(current_round)
+        self.general_remarks = str(general_remarks)
         self.rounds =[]
         self.players=[]
 
-#---------------------------------------------------------------------------------------------
-    def __str__(self):
-            return f"(Tournament name: {self.tournament_name}\n ID: {self.tournament_id})"
-
-    def __repr__(self):
-        return f"Tournament({self.tournament_name}, {self.tournament_id})"
-# -------------------------------------------------------------------------------------------
-    def add_player_objet_in_players_list(self, player):
-        "Add player object in tournament players list attribut."
-        self.players.append(player)
-        
-    def get_players_list(self):
-        return self.players
-#---------------------------------------------------------------------------------------------
-    def add_round_objet_in_rounds_list(self, round):
-        "Add round object in tournament rounds list attribut."
-        self.rounds.append(round)
-
-    def add_match(self, player1, player2, score1, score2):
-        if self.rounds:
-            last_round = self.rounds[-1]
-            last_round.add_match(player1, player2, score1, score2)
-
-
-
-    # def add_new_round(self):
-    #     if len(self.rounds) < self.number_of_rounds:
-    #         self.rounds.append(Round(len(self.rounds) + 1))
+        self.tournaments_database = TinyDB('database/tournaments.json', indent=4)
 
     def format_tournament_in_database(self):
         return {
@@ -54,53 +25,14 @@ class Tournament():
             "end_date":self.end_date,
             "number_of_rounds":self.number_of_rounds,
             "current_round":self.current_round,
-            "general_remarks":self.general_remarks
+            "general_remarks":self.general_remarks,
+            "rounds":self.rounds,
+            "players":self.players
         }
     
     def save_tournament_in_database(self):
         return self.tournaments_database.insert(self.format_tournament_in_database())
-    
-    @staticmethod   # can be called without creating an instance of the class
-    def find_tournament_in_database(tournament_id):
-        tournaments_database = TinyDB('database/tournaments.json')
-        result = tournaments_database.search(where("tournament_id") == tournament_id)   # Search in the database, the tournament corresponding to the id provided as input.
-        if result:
-            return result[0]
-        return "No tournament found in database"
 
-    @staticmethod
-    def update_tournament_in_database(key, value, tournament_id):
-        tournaments_database = TinyDB('database/tournaments.json')
-        tournament = Tournament.find_tournament_in_database(tournament_id)
-        if tournament:
-            return tournaments_database.update({key:value}, where("tournament_id") == tournament_id)    
-        return "Error in parameters"
-
-    @staticmethod
-    def load_all_tournaments_from_database():
+    def load_all_tournaments_from_database(self):
         tournaments_database = TinyDB('database/tournaments.json')
         return tournaments_database.all()
-
-
-
-
-from faker import Faker
-fake = Faker(locale="fr_FR")
-scores = [1, 2, 3, 4]
-idt = [20, 30, 40]
-for _ in range(10):
-    tournament = Tournament(
-        tournament_id= fake.random_element(elements=idt),
-        tournament_name = fake.first_name(),
-        tournament_site = fake.address(), 
-        start_date = fake.date_this_century(),
-        end_date = fake.date_this_century(),
-        current_round = fake.random_element(elements=scores))
-
-    # print(tournament.__str__())
-    # print(tournament.__repr__())
-    # print(tournament.save_tournament_in_database())
-    # print(tournament.find_tournament_in_database(20))
-    # print(tournament.load_all_tournaments_from_database())
-    # print(tournament.update_tournament_in_database("tournament_name", "pepep", 20))
-    # print("-"*10)
