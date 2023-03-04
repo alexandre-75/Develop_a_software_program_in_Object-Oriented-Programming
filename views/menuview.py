feb354a97394901a5b9a486171d761691f614d45
sassy alexandre <sassy.alexandre@gmail.com>
2023-03-04 18:11:49 +0100

    docstring + readme

--- views/menuview.py
+++ views/menuview.py
@@ -4,6 +4,11 @@ class MainMenu():
         pass
 
     def print_main_menu_options(self):
+
+        """
+        Prints the options available in the main menu.
+        """
+        
         print("Welcome to the main menu!")
         print("What would you like to do? Select the desired option by entering its number:")
         print("""
@@ -17,6 +22,13 @@ class MainMenu():
         """)
 
     def enter_a_number_to_select_a_main_menu_option(self):
+
+        """
+        Asks the user to enter a number corresponding to a main menu option.
+        Returns:
+            An integer representing the user's choice of main menu option.
+        """
+
         user_choice = input("Select the desired option (1-7): ")
         if user_choice.isdigit():
             user_choice = int(user_choice)
@@ -25,6 +37,13 @@ class MainMenu():
         print("Invalid input. Please enter a number between 1 and 7.")
 
     def enter_a_number_to_select_a_report_option(self):
+
+        """
+        Asks the user to enter a number corresponding to a report option.
+        Returns:
+            An integer representing the user's choice of report option.
+        """
+
         user_choice = input("\n Select the desired option (1-5): ")
         if user_choice.isdigit():
             user_choice = int(user_choice)
@@ -33,11 +52,28 @@ class MainMenu():
         print("Invalid input. Please enter a number between 1 and 5.")
 
     def quit_the_program_now(self):
+
+        """
+        Asks the user if they want to quit the program.
+        """
+
         print("do you want to quit the program now? (YES or NO) ")
 
     def enter_a_id(self):
+
+        """
+        Asks the user to enter a desired ID.
+        Returns:
+            A string representing the user's desired ID.
+        """
+
         user_choice = input("select the desired ID : ")
         return user_choice
     
     def ergonomics(self):
+
+        """
+        Prints a separator to improve readability.
+        """
+
         print("\n---------------------")
--- views/playerview.py
+++ views/playerview.py
@@ -4,6 +4,12 @@ class PlayerView():
         pass
 
     def summary_of_new_player_created(self, info):
+        """
+        Display a summary of a newly created player.
+        Args:
+            info (dict): A dictionary containing the player's information.
+        """
+
         print("---------- summary of new player created ----------")
         print(f"first name : {info['first_name']}")
         print(f"Last name : {info['last_name']}")
@@ -13,26 +19,69 @@ class PlayerView():
         print(f"Rank : {info['ranking']}")
 
     def player_message_is_saved_in_the_database(self):
+
+        """
+        Display a message indicating that the player information was saved in the database.
+        """
+
         print("player information is correctly saved in the database")
 
     def player_message_not_saved_in_the_database(self):
+
+        """
+        Display a message indicating that the player information was not saved in the database.
+        """
+
         print("player information is not saved in the database")
 
     def display_available_players(self, players):
+
+        """
+        Display a list of available players.
+
+        Args:
+            players (list): A list of dictionaries containing player information.
+        """
+
         print("id, Last Name, First Name")
         for i, player in enumerate(players):
             print(f"{i+1} - {player['last_name']} {player['first_name']} (id: {player['player_id']})")
 
     def display_player_update_options(self, options):
+
+        """
+        Display a list of options for updating a player's information.
+
+        Args:
+            options (list): A list of strings representing the options for updating a player's information.
+        """
+
         for i, option in enumerate(options):
             print(f"[{i+1}] Update {option}")
     
     def exit_save_player(self):
+
+        """
+        Display a message indicating how to stop saving a player's information.
+        """
+
         print("write [quit] to stop saving")
 
     def exit_player_creation_or_continue(self):
+
+        """
+        Display a message asking whether to exit player creation or continue.
+
+        The message also indicates how to exit player creation without saving.
+        """
+
         print("enter the player_id at least")
         print("press [b] to exit without creating a player otherwise press [a] to continue")
     
     def non_editable_value(self):
+
+        """
+        Display a message indicating that a value cannot be changed.
+        """
+
         print("this value cannot be changed")
